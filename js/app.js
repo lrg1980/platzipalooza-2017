4fd0982335df82cf8c1e129703a2b21b37c25eb6
Binweb Online Solutions <admin@binweb.net>
2018-08-21 07:50:27 -0300

    Platzipalooza2017 finalizado con menu movil

--- js/app.js
+++ js/app.js
@@ -4,3 +4,13 @@ $('[data-toggle-dia]').click(function onclick (ev) {
     const panel = $(this).data('toggleDia')
     $('#lineup-tabs').foundation('selectTab', panel)
 })
+
+const $offCanvas = $('#offCanvas')
+const $sticky = $('#sticky')
+
+$offCanvas.find('li').click(function (ev) {
+    $offCanvas.foundation('close')
+    setTimeout(function () {
+        $sticky.css('left', '0px')
+    }, 300)    
+})
