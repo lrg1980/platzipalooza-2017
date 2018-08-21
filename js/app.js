be65301f55afff9e8d8b8481fec85af9fa37c061
Binweb Online Solutions <admin@binweb.net>
2018-08-21 06:22:12 -0300

    Navegación en LineUp desde menu y submenu

--- js/app.js
+++ js/app.js
@@ -1,1 +1,6 @@
 $(document).foundation()
+
+$('[data-toggle-dia]').click(function onclick (ev) {
+    const panel = $(this).data('toggleDia')
+    $('#lineup-tabs').foundation('selectTab', panel)
+})
